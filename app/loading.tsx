38029d7c819a8ad1925b2089b426e6f26a28bbc4
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-03-12 19:59:15 +0530

    Loading modified

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className="min-h-screen dark:bg-slate-800 gap-6 flex items-center justify-center">
+    <div className="min-h-screen bg-gray-950 gap-6 flex items-center justify-center">
       <div className="w-36 h-36 border-8 border-dashed rounded-full animate-spin border-transparent bg-gradient-to-r from-blue-400 to-green-400 p-1">
-        <div className="w-full h-full bg-slate-800 rounded-full" />
+        <div className="w-full h-full bg-gray-950 rounded-full" />
       </div>
     </div>
   )
